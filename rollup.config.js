c8bcd94a59d16c33af93843531d9c874f0510bb2
Gugu <gugunnn@gmail.com>
2025-01-21 03:01:22 +0200

    restctured the repo for a svelte-formique variant

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import babel from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
 
 export default {
-  input: 'src/js/formique.js',
+  input: 'src/js/formique-svelte.js',
   output: [
     {
       file: 'dist/js/formique.cjs.js',
