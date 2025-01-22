7ce187be3f61914fd678dc652311e70b8a11557d
Gugu <gugunnn@gmail.com>
2025-01-22 20:56:21 +0200

    update textarea call in all versions

--- src/js/formique-svelte.js
+++ src/js/formique-svelte.js
@@ -424,7 +424,7 @@ renderField(type, name, label, validate, attributes, options) {
         'email': this.renderEmailField,
         'number': this.renderNumberField,
         'password': this.renderPasswordField,
-        'password': this.renderTextAreaField,
+        'textarea': this.renderTextAreaField,
         'tel': this.renderTelField,
         'date': this.renderDateField,
         'time': this.renderTimeField,
