ec71d61426a9bede9037b751a19bddf1e0858664
Gugu <gugunnn@gmail.com>
2025-01-30 11:21:17 +0200

    Updated vue js formique bundle

---
+++ integrations/vue/npm_standard/test_vue_formique/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>Testing vue-formique</h1>
+    <VueFormique />
+  </div>
+</template>
+
+<script>
+import { VueFormique } from 'vue-formique';
+
+export default {
+  name: 'App',
+  components: {
+    VueFormique,
+  },
+};
+</script>
+
+
